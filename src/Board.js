f85dcd78ce95407624f5cd5be8ca18f7c30ae325
tuan-bacoor <huynhtuan@bacoor.co>
2019-08-21 13:12:23 +0700

    add

--- src/Board.js
+++ src/Board.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useDrop, useDrag } from "react-dnd";
 import ItemTypes from "./ItemTypes";
+import uniqid from 'uniqid';
 const style = {
   width: 400,
   height: 400,
@@ -11,21 +12,28 @@ const style = {
 };
 const Board = () => {
   const [boxPos, setBoxPos] = useState([0, 0]);
+  const [listEl,setListEl] = useState([]);
   const [{ canDrop, isOver }, drop] = useDrop({
-    accept: ItemTypes.BOX,
+    accept: ItemTypes.BOARD,
     drop: (item, monitor) => {
-      const newPos = [boxPos[0]+monitor.getDifferenceFromInitialOffset().x,boxPos[1]+monitor.getDifferenceFromInitialOffset().y];
-      setBoxPos(newPos);
+      console.log("item", item);
+      const newPos = [boxPos[0] + monitor.getDifferenceFromInitialOffset().x, boxPos[1] + monitor.getDifferenceFromInitialOffset().y];
+      setBoxPos(newPos);  
       console.log("drop", monitor.getDifferenceFromInitialOffset());
+      console.log("drop result",monitor.getDropResult());
     },
     collect: monitor => ({
       isOver: monitor.isOver(),
       canDrop: monitor.canDrop()
     })
   });
+  const [id, setId] = useState(uniqid());
   return (
-    <div ref={drop} style={{ ...style }}>
-      <Box boxPos={boxPos} />
+    <div className="container">
+      <InitInputText />
+      <div ref={drop} style={{ ...style }}>
+        <Box id={id} boxPos={boxPos} />
+      </div>
     </div>
   );
 };
@@ -36,12 +44,13 @@ const BoxStyle = {
   position: "absolute",
   cursor: "move"
 };
-const Box = ({ boxPos:[x,y] }) => {
+const Box = ({ boxPos: [x, y], id }) => {
   const [{ isDragging }, drag] = useDrag({
-    item: { type: ItemTypes.BOX },
+    item: { id: id, type: ItemTypes.BOARD },
     end: (item, monitor) => {
       const dropResult = monitor.getDropResult();
       if (item && dropResult) {
+        console.log("drop result", item);
       }
     },
     collect: monitor => ({
@@ -49,6 +58,46 @@ const Box = ({ boxPos:[x,y] }) => {
     })
   });
   const opacity = isDragging ? 0.4 : 1;
-  return <div ref={drag} style={{ ...BoxStyle,left:x,top:y, opacity }} />;
+  return <div ref={drag} style={{ ...BoxStyle, left: x, top: y, opacity }} />;
 };
+
+const InputTextStyle = {
+  position: "absolute",
+  cursor: "move"
+};
+const InputText = ({ pos: [x, y], id }) =>{
+  const [{ isDragging }, drag] = useDrag({
+    item: { id:id,type: ItemTypes.BOARD, },
+    end: (item, monitor) => {
+      const dropResult = monitor.getDropResult();
+      if (item && dropResult) {
+        console.log("drop result", item);
+      }
+    },
+    collect: monitor => ({
+      isDragging: monitor.isDragging()
+    })
+  });
+  const opacity = isDragging ? 0.4 : 1;
+  return <input type="text" ref={drag} style={{ ...InputTextStyle,left:x,top:y, opacity }} />;
+}
+const InitInputTextStyle = {
+  cursor: "move"
+};
+const InitInputText = () => {
+  const [{ isDragging }, drag] = useDrag({
+    item: { type: ItemTypes.BOARD,cpn:'INPUT_TEXT'},
+    end: (item, monitor) => {
+      const dropResult = monitor.getDropResult();
+      if (item && dropResult) {
+        console.log("drop result", item);
+      }
+    },
+    collect: monitor => ({
+      isDragging: monitor.isDragging()
+    })
+  });
+  const opacity = isDragging ? 0.4 : 1;
+  return <input type="text" ref={drag} style={{ ...InitInputTextStyle, opacity }} />;
+}
 export default Board;
--- src/ItemTypes.js
+++ src/ItemTypes.js
@@ -1,3 +1,5 @@
 export default {
   BOX: 'box',
+  BOARD:'board',
+  INPUT_TEXT:'input_text'
 }
